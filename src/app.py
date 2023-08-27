5a6e7b57da0ce23f9fff83c8c579294862679c7a
Kristoffer <kristoffer.eik@uib.no>
2023-08-27 13:25:38 +0200

    new example

--- src/app.py
+++ src/app.py
@@ -1,24 +1,35 @@
-from dash import Dash, html, dcc, callback, Output, Input
+from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
-import pandas as pd
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+df = px.data.gapminder()
 
 app = Dash(__name__)
+server = app.server
 
-app.layout = html.Div([
-    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
-    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
-    dcc.Graph(id='graph-content')
-])
-
-@callback(
-    Output('graph-content', 'figure'),
-    Input('dropdown-selection', 'value')
+app.layout = html.Div(
+    [
+        html.H4("Country's key performance analytics"),
+        html.P("Select data on y-axis:"),
+        dcc.Dropdown(
+            id="y-axis",
+            options=["lifeExp", "pop", "gdpPercap"],
+            value="gdpPercap",
+        ),
+        dcc.Graph(id="graph"),
+    ]
 )
-def update_graph(value):
-    dff = df[df.country==value]
-    return px.line(dff, x='year', y='pop')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+@app.callback(
+    Output("graph", "figure"),
+    Input("y-axis", "value"),
+)
+def display_area(y):
+    countries = df.country.drop_duplicates().sample(n=10, random_state=42)
+    dff = df[df.country.isin(countries)]
+    fig = px.area(dff, x="year", y=y, color="continent", line_group="country")
+    return fig
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
